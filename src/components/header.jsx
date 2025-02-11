951aeae4bdde60a2380f713b5a1a34b8824bf154
Bukunmi Adigun <105093417+Bukunmi16@users.noreply.github.com>
2025-02-11 14:35:49 +0100

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
     return(
         <header>
             <img src="chef-claude-logo.png" alt="chef-claude-logo" />
-            <h1>Chef Bukunmi</h1>
+            <h1>Chef Glory</h1>
         </header>
     )
 }
